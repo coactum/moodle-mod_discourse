be934e5081daaef4d9add0b9804d45c6a3f8d17a
Daniel Nolte <>
2021-08-13 11:51:43 +0200

    feat(version): increasing version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_discourse';
 $plugin->release = '1.0.0';
-$plugin->version = 2021081100;
+$plugin->version = 2021081300;
 $plugin->requires = 2020061507;
 $plugin->maturity = MATURITY_BETA;
